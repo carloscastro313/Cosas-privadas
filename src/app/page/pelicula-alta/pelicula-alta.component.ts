55a3a091d57cf6456252952237381761e5390c93
Carlos Castro <castrocarlos313@gmail.com>
2021-05-05 16:57:11 -0300

    parte 3 incompleta

--- src/app/page/pelicula-alta/pelicula-alta.component.ts
+++ src/app/page/pelicula-alta/pelicula-alta.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-pelicula-alta',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeliculaAltaComponent implements OnInit {
 
-  constructor() { }
+  public forma : FormGroup;
+
+  constructor(private fb:FormBuilder,private storage:StoreService) { }
 
   ngOnInit(): void {
+    this.forma = this.fb.group({
+      'nombre':['',Validators.required],
+      'apellido':['',Validators.required],
+      'edad': ['',[Validators.required,Validators.min(18),Validators.max(99)]],
+      'correo':['',[Validators.required,Validators.email]],
+      'pais':['',Validators.required],
+  })
+}
+
+  seleccion(value){
+    // console.log(value);
+    this.forma.get('pais').setValue(value);
   }
 
+  public async aceptar():Promise<void>
+  {
+    if(this.forma.valid)
+    {
+
+    }
+    else{
+      window.alert("Por favor, complete correctamente los campos antes de enviar el formulario.");
+    }
+  }
 }
